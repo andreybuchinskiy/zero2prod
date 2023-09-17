4e359e17ba1ec21512e6fca2ee2c8a1bf0dcd54c
Andrey Buchinskiy <andreybuchinskiy@gmail.com>
2023-09-16 20:39:41 -0600

    completed chapter 10

---
+++ migrations/20230916231951_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+  'f38c34f9-e84d-4233-90f0-194ef3df4011',
+  'admin',
+  '$argon2id$v=19$m=15000,t=2,p=1$rf67J4RIrgNayKKOzGOG3g$ZXFyHLMFphCnAxERkhP0P0qtEVND9d6gBY/NDSa1b5M'
+);
